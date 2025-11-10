d35adb8e623afb631e54bbf8ebbd3a3a89053606
Juan Victor <juanvictoficial@gmail.com>
2025-11-10 15:33:00 -0300

    fix(style): Corrige erros de estilo usando ESLint --fix

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const app = require("./index");
-const PORT = 8080;
+const app = require('./index')
+const PORT = 8080
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
-});
+  console.log(`Servidor rodando em http://localhost:${PORT}`)
+})
